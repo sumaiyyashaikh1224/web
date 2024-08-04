a97b007cf4438ba7c726139d7435f0f0a19c4586
sumaiyya shaikh <sumaiyyashaikha8482@gmail.com>
2024-08-04 12:03:34 +0530

    script.js

---
+++ scope.js/Scope.js
@@ -0,0 +1,13 @@
+let a  = 4;
+function add () {
+    let c = a +2;
+    console.log(c);
+}
+let age = 17;
+if (age < 18) {
+    let message = "you cannot drive";
+    console.log(message);
+}else{
+
+}
+console.log(message);
